03f790b88a1aa1e695c4305166bbc034e261e0ad
Jona <jona@pop-os.localdomain>
2020-10-29 16:17:29 +0100

    semi working input.c after johannes helped a bit

--- Input.c
+++ Input.c
@@ -5,23 +5,55 @@
 #include <string.h>
 
 #define LINE_LEN 500
-#define NAME_LEN 40
 
 int main(void){
-char line[LINE_LEN], inname[NAME_LEN];
+
+char ord[10][50];
+ord[0][0] = 'c';
+
+char line[LINE_LEN];
 FILE *inp;
 inp = fopen("Data/da_ddt-ud-train", "r");
 
 int i = 0;
 
 char *status = fgets(line, LINE_LEN, inp);
-while(status != 0){
+//while(status != 0){
+while(i < 10 && status != 0){
+    printf("%d\n",i);
     if (line[strlen(line) - 1] == '\n'){
         line[strlen(line) - 1] = '\0';
     }
-    printf("%d>> %s\n\n", ++i, line);
-
+    char *token = strtok(line, "\t");
+    token = strtok(NULL, "\t");
+    printf("Token is: %s\n", token);
+    
+    if(token == NULL){
+        i++;
+        status = fgets(line, LINE_LEN, inp);
+        continue;
+    }
+    int f;
+    for(f = 0; f < (int)strlen(token); f++){
+        printf("%d %d\n", i, f);
+        ord[i][f] = token[f];
+        
+    }
+    printf("%d",f);
+    ord[i][f] = '\0';
+    
+    
+    //printf("%d>> %s\n\n", ++i, line);
+    
     status = fgets(line, LINE_LEN, inp);
+    i++;
+}
+
+for(int j = 0; j < 10; j++){
+    printf("%d", j);
+    puts(ord[j]);
+    printf("\n");
+    
 }
 
 return (0);
