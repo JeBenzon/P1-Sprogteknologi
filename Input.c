cf799b6ee5fb8c6fdec186a66288e437594209dd
FredemandDK <fskontorp@gmail.com>
2020-11-02 11:00:35 +0100

    Optimering af programmets udseende

--- Input.c
+++ Input.c
@@ -5,51 +5,53 @@
 #include <string.h>
 
 #define LINE_LEN 500
+#define WORDS_COUNT 10
+#define CHAR_COUNT 50
 
 int main(void){
 
-char ord[10][50];
-char line[LINE_LEN];
-FILE *inp = fopen("Data/da_ddt-ud-train", "r");
-int i = 0;
-char *status = fgets(line, LINE_LEN, inp);
+    char ord[WORDS_COUNT][CHAR_COUNT];
+    char line[LINE_LEN];
+    FILE *inp = fopen("Data/da_ddt-ud-train", "r");
+    char *status = fgets(line, LINE_LEN, inp);
+    int i = 0;
 
-while(i < 10 && status != 0){
+    while(i < WORDS_COUNT && status != 0){
 
-    if (line[strlen(line) - 1] == '\n'){
-        line[strlen(line) - 1] = '\0';
-    }
-    //Læser 1 linje ind i Token
-    char *token = strtok(line, "\t");
-    //sætter Token til at være det næste ord i Token linjen.
-    token = strtok(NULL, "\t");
-    
-    
-    //Tjekker om Token er tom
-    if(token == NULL){
-        i++;
-        status = fgets(line, LINE_LEN, inp);
-        continue;
-    }
+        if (line[strlen(line) - 1] == '\n'){
+            line[strlen(line) - 1] = '\0';
+        }
 
-    int f;
-    //læser ordet fra Token og sætter det ind i Ord array
-    for(f = 0; f < (int)strlen(token); f++){
+        //Læser alle ord fra linjen ind i Token, så består nu af flere ord.
+        char *token = strtok(line, "\t");
+        //skipper et ord i token.
+        token = strtok(NULL, "\t");
         
-        ord[i][f] = token[f];
+        //Tjekker om Token er tom
+        if(token == NULL){
+            i++;
+            status = fgets(line, LINE_LEN, inp);
+            continue;
+        }
+
+        int f;
+        //læser ordet fra Token og sætter det ind i Ord array
+        for(f = 0; f < (int)strlen(token); f++){
+            
+            ord[i][f] = token[f];
+        }
+        ord[i][f] = '\0';
+        
+        //Læser ny linje ind i status
+        status = fgets(line, LINE_LEN, inp);
+        i++;
     }
-    ord[i][f] = '\0';
-    
-    //Læser ny linje ind i status
-    status = fgets(line, LINE_LEN, inp);
-    i++;
-}
 
-for(int j = 0; j < 10; j++){
+    for(int j = 0; j < WORDS_COUNT; j++){
+        //puts printer ordet ud
+        puts(ord[j]);
     
-    puts(ord[j]);
-    
-}
+    }   
 
-return (0);
+    return (0);
 }
